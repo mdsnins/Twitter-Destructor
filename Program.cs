4913c7f237cca9561a114ef04e2ff0772ae3e239
Payload <ddongji0316@naver.com>
2019-01-19 20:39:22 +0900

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+
+namespace TwitDestructor
+{
+    class Program
+    {
+        const string API_KEY = "";
+        const string API_SECRET = "";
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                TwitterClient twc = new TwitterClient(API_KEY, API_SECRET);
+                
+                Console.WriteLine("Requesting Twitter Tokens..");
+                twc.request_token();
+
+                Console.Write("Complete Twitter auth, and enter the pin > ");
+                System.Diagnostics.Process.Start(twc.authenticate_url());
+
+                string pin = Console.ReadLine();
+                Console.WriteLine("Verifying pin inputs..");
+                twc.authenticate(pin);
+                
+
+                dynamic cred = JObject.Parse(twc.oauth_send("https://api.twitter.com/1.1/account/verify_credentials.json", "GET"));
+                Console.WriteLine("Welcome, @" + cred.screen_name.ToString() + "!");
+                Console.WriteLine("Type the path of 'tweet.js' or drag&drop it!");
+                Console.Write("> ");
+                string archive_path = Console.ReadLine();
+
+                if (!File.Exists(archive_path))
+                    throw new Exception("No such file!");
+
+                string contents;
+                using (StreamReader sr = new StreamReader(archive_path, System.Text.Encoding.UTF8))
+                    contents = sr.ReadToEnd();
+
+                contents = "{\"data\":" + contents.Substring(25).Replace(" ", "").Replace("\r", "").Replace("\n", "") + "}";
+                dynamic tweets = JObject.Parse(contents);
+
+                Console.WriteLine("{0} tweets loaded!", tweets.data.Count);
+                List<String> d_err = new List<string>();
+
+                foreach (dynamic t in tweets)
+                {
+                    try
+                    {
+                        twc.remove_tweet(t.id_str.ToString());
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Error deleting tweet(id=" + t.id_str.ToString());
+                        d_err.Add(t.id_str.ToString());
+                    }
+                }
+
+                while(d_err.Count > 0)
+                {
+                    
+                    Console.Write("There were {0} errors, retry? (y/n) > ");
+                    string answer = Console.ReadLine();
+                    if (answer.ToLower() == "n")
+                        break;
+                    else if (answer.ToLower() != "y")
+                        continue;
+
+                    List<String> n_err = new List<string>();
+                    foreach(string tid in d_err)
+                    {
+                        try
+                        {
+                            twc.remove_tweet(tid);
+                        }
+                        catch
+                        {
+                            Console.WriteLine("Error deleting tweet(id=" + tid);
+                           n_err.Add(tid);
+                        }
+                    }
+
+                    d_err = n_err;
+                }
+
+                Console.WriteLine("Done!");
+                Console.ReadLine();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Exception while executing: " + e.Message);
+            }
+        }
+    }
+}
---
+++ TwitterClient.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace TwitDestructor
+{
+    class TwitterClient
+    {
+        private string api_key;
+        private string api_secret;
+
+        private string token = "";
+        private string token_secret ="";
+
+
+        private string ver = "1.0";
+        private string sign_method = "HMAC-SHA1";
+
+        public TwitterClient(string consumer_key, string consumer_secret)
+        {
+            api_key = consumer_key;
+            api_secret = consumer_secret;
+        }
+
+        public void set_dev_token(string dtoken, string dsecret)
+        {
+            token = dtoken;
+            token_secret = dsecret;
+        }
+
+        private string generate_nonce()
+        {
+            return Convert.ToBase64String(new ASCIIEncoding().GetBytes(DateTime.Now.Ticks.ToString()));
+        }
+
+        private string get_timestamp()
+        {
+            var timeSpan = DateTime.UtcNow - new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+            return Convert.ToInt64(timeSpan.TotalSeconds).ToString();
+
+        }
+
+        public void remove_tweet(string twt_id)
+        {
+            try
+            {
+                oauth_send("https://api.twitter.com/1.1/statuses/destroy/" + twt_id + ".json");
+            }
+            catch
+            {
+                throw new Exception("Failed in deleting tweet!");
+            }
+        }
+        
+        public void request_token()
+        {
+            try
+            {
+                string response = oauth_send("https://api.twitter.com/oauth/request_token");
+                string[] data = response.Split('&');
+
+                token = data[0].Substring(12);
+                token_secret = data[1].Substring(19);
+            }
+            catch
+            {
+                throw new Exception("Failed in requesting token!");
+            }
+        }
+
+        public string authenticate_url()
+        {
+            if (token.Length > 0)
+                return "https://api.twitter.com/oauth/authenticate?oauth_token=" + token;
+            else
+                throw new Exception("Token isn't generated");
+        }
+
+        public void authenticate(string pin)
+        {
+            try
+            {
+                Dictionary<string, string> param = new Dictionary<string, string>();
+                param["oauth_verifier"] = pin;
+                string response = oauth_send("https://api.twitter.com/oauth/access_token", "POST", param);
+                string[] data = response.Split('&');
+
+                token = data[0].Substring(12);
+                token_secret = data[1].Substring(19);
+            }
+            catch
+            {
+                throw new Exception("Failed in authenticate!");
+            }
+        }
+           
+        public string oauth_send(string target, string method="POST", Dictionary<string, string> user_data = null)
+        {
+            string send_string = "";
+
+            // unique request details
+            string nonce = generate_nonce();
+            string timestamp = get_timestamp();
+
+            // create oauth signature
+            string base_format = "oauth_consumer_key={0}&oauth_nonce={1}&oauth_signature_method={2}" +
+                            "&oauth_timestamp={3}&oauth_token={4}&oauth_version={5}";
+
+
+            string param_string = string.Format(base_format, api_key, nonce, sign_method, timestamp, token, ver);
+
+            if (user_data != null)
+                foreach (KeyValuePair<String, String> kv in user_data)
+                {
+                    param_string = param_string + "&" + kv.Key + "=" + Uri.EscapeDataString(kv.Value);
+                    send_string = send_string + "&" + kv.Key + "=" + Uri.EscapeDataString(kv.Value);
+                }
+            if (send_string.Length > 0)
+                send_string = send_string.Substring(1);
+            param_string = method + "&" + Uri.EscapeDataString(target) + "&" + Uri.EscapeDataString(param_string);
+
+            var compositeKey = Uri.EscapeDataString(api_secret) + "&" + Uri.EscapeDataString(token_secret);
+
+            string oauth_signature;
+            using (HMACSHA1 hasher = new HMACSHA1(ASCIIEncoding.ASCII.GetBytes(compositeKey)))
+            {
+                oauth_signature = Convert.ToBase64String(
+                    hasher.ComputeHash(ASCIIEncoding.ASCII.GetBytes(param_string)));
+            }
+
+            // create the request header
+            string headerFormat = "OAuth oauth_nonce=\"{0}\", oauth_signature_method=\"{1}\", " +
+                               "oauth_timestamp=\"{2}\", oauth_consumer_key=\"{3}\", " +
+                               "oauth_token=\"{4}\", oauth_signature=\"{5}\", " +
+                               "oauth_version=\"{6}\"";
+
+            string authHeader = string.Format(headerFormat,
+                                    Uri.EscapeDataString(nonce),
+                                    Uri.EscapeDataString(sign_method),
+                                    Uri.EscapeDataString(timestamp),
+                                    Uri.EscapeDataString(api_key),
+                                    Uri.EscapeDataString(token),
+                                    Uri.EscapeDataString(oauth_signature),
+                                    Uri.EscapeDataString(ver)
+                            );
+
+            // make the request
+            ServicePointManager.Expect100Continue = false;
+
+            if (method == "GET")
+                target = target + "?" + send_string;
+            WebRequest request = WebRequest.Create(target);
+            request.Headers.Add("Authorization", authHeader);
+            request.Method = method;
+            request.ContentType = "application/x-www-form-urlencoded";
+
+            if (method == "POST")
+            {
+                byte[] param_data = Encoding.UTF8.GetBytes(send_string);
+                request.ContentLength = param_data.Length;
+                Stream data_stream = request.GetRequestStream();
+                data_stream.Write(param_data, 0, param_data.Length);
+                data_stream.Close();
+            }
+
+
+            WebResponse response = request.GetResponse();
+            string responseData = new StreamReader(response.GetResponseStream()).ReadToEnd();
+            return responseData;
+        }
+    }
+}
